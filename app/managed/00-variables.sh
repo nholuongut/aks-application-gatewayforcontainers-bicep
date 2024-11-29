27a2355527cf18362581cdddfc9ae441bdb43a79
Nho Luong <nholu@nholuongs-MacBook-Pro.local>
2024-11-29 10:44:25 +0700

    Welcome to my repositories DevOps blogs

---
+++ app/managed/00-variables.sh
@@ -0,0 +1,23 @@
+# Certificate Manager
+cmNamespace="cert-manager"
+cmRepoName="jetstack"
+cmRepoUrl="https://charts.jetstack.io"
+cmChartName="cert-manager"
+cmReleaseName="cert-manager"
+cmVersion="v1.14.0"
+
+# Application Load Balancer 
+applicationLoadBalancerName="alb"
+applicationLoadBalancerNamespace="alb-infra"
+
+# Demo
+namespace="agc-demo"
+gatewayName="echo-gateway"
+issuerName="letsencrypt"
+httpRouteName="echo-route"
+
+# Ingress and DNS
+dnsZoneName="nholuongut.com"
+dnsZoneResourceGroupName="DnsResourceGroup"
+subdomain="shogunagc"
+hostname="$subdomain.$dnsZoneName"
